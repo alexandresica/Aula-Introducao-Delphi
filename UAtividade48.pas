0edb7d7a802d548276b5b4c3e19c6cb47ad2d23c
alexandresica <alexandresicavalcante@gmail.com>
2026-05-13 22:39:45 -0300

    update atv 48

--- UAtividade48.pas
+++ UAtividade48.pas
@@ -26,29 +26,41 @@ implementation
 
 procedure TfrmAtividade48.FormShow(Sender: TObject);
 begin
-begin
+
  Timer.Interval := 10;
  Timer.Enabled := True;
-end;
+
 end;
 
 procedure TfrmAtividade48.TimerTimer(Sender: TObject);
 var
-nDigitado : String;
+nDigitado, controle : String;
   contador: Integer;
   numero, soma, media : Real;
 
 begin
+
+contador := 0;
+controle := '1';
+
 repeat
   nDigitado := InputBox('Atividade 48',
     'Digite um numero ','');
-    contador++
+    Inc(contador);
+
     numero := StrToFloat(nDigitado);
-
     soma := soma + numero;
-    media := soma / contador
 
-until nDigitado = '0' ;
+
+until controle = '0' ;
+
+media := soma / contador;
+
+MessageBox(Application.Handle,Pchar(
+'Quantidade de numeros digitados: ' +IntToStr(contador)+#13+
+'Soma: '+FloatToStr(soma)+#13+
+'Media: '+FloatToStr(media)), 'CSM0',MB_OK);
+  Close;
 end;
 
 end.
